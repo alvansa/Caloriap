e3599fedac7e650d863314f5a315ca0bf71f4615
Diexego <111141115+Diexego@users.noreply.github.com>
2023-07-18 00:53:57 -0400

    Update busqueda.dart
    nuevo ejemplo

--- lib/page-1/busqueda.dart
+++ lib/page-1/busqueda.dart
@@ -3,7 +3,7 @@ import 'package:flutter/gestures.dart';
 import 'dart:ui';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:myapp/utils.dart';
-
+//ejemplonuevo
 class busqueda extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
