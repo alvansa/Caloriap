565d2603a099bc06ec6893f4fd2304b2c7739165
Alvaro Soto <alvarosotoaa@gmail.com>
2023-07-21 00:46:41 -0400

    registro de usuario

--- lib/controller/con_actualizar_alimento.dart
+++ lib/controller/con_actualizar_alimento.dart
@@ -13,7 +13,7 @@ class con_actualizar_alimento {
       String id_al,
       String nombre,
       String calorias,
-      String azuccares,
+      String azucares,
       String proteina,
       String sodio,
       String grasa_total,
@@ -22,26 +22,43 @@ class con_actualizar_alimento {
       String porcion) async {
     final alimento = model_alimento();
     final id_al_int = int.parse(id_al);
-    final calorias_int = calorias.isNotEmpty ? int.parse(calorias) : 0;
-    final azuccares_int = azuccares.isNotEmpty ? int.parse(azuccares) : 0;
-    final proteina_int = proteina.isNotEmpty ? int.parse(proteina) : 0;
-    final sodio_int = sodio.isNotEmpty ? int.parse(sodio) : 0;
-    final grasa_total_int = grasa_total.isNotEmpty ? int.parse(grasa_total) : 0;
-    final h_de_c_int = h_de_c.isNotEmpty ? int.parse(h_de_c) : 0;
-    final colesterol_int = colesterol.isNotEmpty ? int.parse(colesterol) : 0;
-    final porcion_int = porcion.isNotEmpty ? int.parse(porcion) : 0;
+    double calorias_int = 0;
+    double proteinas_int = 0;
+    double grasas_totales_int = 0;
+    double hidratos_carbono_int = 0;
+    double azucares_int = 0;
+    double colesterol_int = 0;
+    double sodio_int = 0;
+    double porcion_int = 0;
+    if (calorias.isNotEmpty) calorias_int = double.parse(calorias);
+    if (proteina.isNotEmpty) proteinas_int = double.parse(proteina);
+    if (grasa_total.isNotEmpty) grasas_totales_int = double.parse(grasa_total);
+    if (h_de_c.isNotEmpty) hidratos_carbono_int = double.parse(h_de_c);
+    if (azucares.isNotEmpty) azucares_int = double.parse(azucares);
+    if (colesterol.isNotEmpty) colesterol_int = double.parse(colesterol);
+    if (sodio.isNotEmpty) sodio_int = double.parse(sodio);
+    if (porcion.isNotEmpty) porcion_int = double.parse(porcion);
     final act_alimento = alimento.actualizar_alimento(
         id_al_int,
         nombre,
         calorias_int,
-        azuccares_int,
-        proteina_int,
+        azucares_int,
+        proteinas_int,
         sodio_int,
-        grasa_total_int,
-        h_de_c_int,
+        grasas_totales_int,
+        hidratos_carbono_int,
         colesterol_int,
         porcion_int);
 
     return act_alimento;
+  }
+
+  List<dynamic> cargar_datos(int id_al) {
+    final alimento = model_alimento();
+    final nombre_alimento = alimento.datos_alimento(id_al);
+    List<dynamic> datos = [];
+    nombre_alimento.then((value) => datos = value!);
+    print(datos);
+    return datos;
   }
 }
